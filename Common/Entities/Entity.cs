86b49ec915b812e98918ca9fde6e7e76bdc1a418
GuyFaux <nmarston@internode.on.net>
2014-04-15 10:34:30 +0930

    Initial Commit

---
+++ Common/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDSF.Common
+{
+    public interface Entity
+    {
+        public long EntityID { get; set; }
+
+        public Entity Parent { get; set; }
+    }
+}
